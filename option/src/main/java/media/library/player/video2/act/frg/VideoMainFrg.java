a0ffd906daca414d9f875da64cb759fd00a59f9c
guom <452693688@qq.com>
2025-11-21 19:11:27 +0800

    修改video2

---
+++ option/src/main/java/media/library/player/video2/act/frg/VideoMainFrg.java
@@ -0,0 +1,235 @@
+package media.library.player.video2.act.frg;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.images.imageselect.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.media3.ui.PlayerView;
+import androidx.viewpager2.widget.ViewPager2;
+import media.library.player.bean.TestVideoUrl;
+import media.library.player.bean.VideoPlayVo;
+import media.library.player.video2.able.OnVideoData2;
+import media.library.player.video2.able.OnVideoLoading;
+import media.library.player.video2.able.OnVideoOperate2;
+import media.library.player.video2.frg2.VideoBaseFrg0;
+import media.library.player.video2.manger.ShortVideoManager2;
+import media.library.player.view.CustomExoPlayer;
+import media.library.player.view.VideoPageRl2;
+
+
+public class VideoMainFrg extends Fragment implements OnVideoOperate2, OnVideoData2 {
+    protected FragmentActivity act;
+    protected Fragment relyFrg;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.media_frg_main_video, container, false);
+        return view;
+
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        act = getActivity();
+        relyFrg = this.getParentFragment();
+        setViewInit(view, savedInstanceState);
+        setClick();
+    }
+
+    private ViewPager2 viewPager2;
+    private VideoPageRl2 videoPageRl;
+    private View viewDataLoading;
+    private ShortVideoManager2 videoPlaysManager;
+
+    private void setViewInit(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        viewPager2 = view.findViewById(R.id.view_pager);
+        viewDataLoading = view.findViewById(R.id.view_data_loading);
+        videoPageRl = view.findViewById(R.id.view_root);
+
+        videoPlaysManager = new ShortVideoManager2();
+        videoPlaysManager.setOnVideoLoading(new OnVideoLoading() {
+            @Override
+            public void onVideoLoadingUp(int index, int indexUp, VideoBaseFrg0 videoFrgNow, VideoBaseFrg0 videoFrgUp) {
+                //预加载上一页数据
+                videoFrgUp.setVideoDataPlay(indexUp);
+            }
+
+            @Override
+            public void onVideoLoadingDow(int index, int indexDow, VideoBaseFrg0 videoFrgNow, VideoBaseFrg0 videoFrgDow) {
+                //预加载下一页数据
+                videoFrgDow.setVideoDataPlay(indexDow);
+            }
+
+            @Override
+            public ArrayList<VideoBaseFrg0> getPages() {
+                ArrayList pages = new ArrayList<VideoBaseFrg0>();
+               /* pages.add(VideoTabMainFrg5());
+                pages.add(VideoTabMainFrg5());
+                pages.add(VideoTabMainFrg5());
+                pages.add(VideoTabMainFrg5());
+                pages.add(VideoTabMainFrg5());
+                pages.add(VideoTabMainFrg5());
+                pages.add(VideoTabMainFrg5());*/
+                return pages;
+            }
+        });
+        videoPlaysManager.setView(this, viewPager2);
+    }
+
+    private void setClick() {
+        videoPageRl.setOnVerticalListener(new VideoPageRl2.OnVerticalListener() {
+            @Override
+            public void onVertical() {
+                if (isMoreReq) {
+                    return;
+                }
+                if (videos.size() == 0) {
+                    return;
+                }
+                if (videoPlaysManager.getResumeIndex() != (videos.size() - 1)) {
+                    return;
+                }
+                setDataMore(true);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        videoPlaysManager.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        videoPlaysManager.onPause();
+        handlerData.sendEmptyMessageDelayed(0, 1 * 1000);
+    }
+
+    @Override
+    public void onDestroy() {
+        videoPlaysManager.onDestroy();
+        super.onDestroy();
+    }
+
+    private boolean isMoreReq = false;
+
+    private void setDataMore(boolean isShowView) {
+        if (isMoreNot) {
+            //没有更多了
+            return;
+        }
+        if (isShowView) {
+            viewDataLoading.setVisibility(View.VISIBLE);
+        }
+        if (isMoreReq) {
+            return;
+        }
+        isMoreReq = true;
+        //数据加载中
+        handlerData.sendEmptyMessageDelayed(1, 3 * 1000);
+    }
+
+    //
+    //设置页面
+    private void setPageData(String url) {
+        videos.clear();
+        videos.add(url);
+        videoPlaysManager.setUpdateDataSize(videos.size());
+        VideoBaseFrg0 videoFrg = videoPlaysManager.setPageCurrentItem0();
+        videoFrg.setVideoDataPlay(-1);
+        videoFrg.setDataUpdate(0);
+    }
+
+    //设置页面
+    private void setPageDataMore(String url) {
+        if (videos.contains(url)) {
+            return;
+        }
+        videos.add(url);
+        videoPlaysManager.setUpdateDataSize(videos.size());
+    }
+
+    //
+    private ArrayList videos = new ArrayList<String>();
+    private HandlerData handlerData = new HandlerData();
+    //true 没有更多了
+    private boolean isMoreNot;
+
+
+    class HandlerData extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case 0:
+                    String url = new TestVideoUrl().video1;
+                    isMoreNot = false;
+                    setPageData(url);
+                    break;
+                case 1:
+                    //加载一条数据
+                    isMoreReq = false;
+                    isMoreNot = true;
+                    viewDataLoading.setVisibility(View.GONE);
+                    url = new TestVideoUrl().video4;
+                    setPageDataMore(url);
+                    break;
+            }
+        }
+    }
+
+    //
+    @Override
+    public CustomExoPlayer getExoPlayer(Integer pageIndex, String videoUrl) {
+        return videoPlaysManager.getExoPlayer(pageIndex);
+    }
+
+    @Override
+    public void setViewPageSlide(boolean isSlide) {
+        videoPlaysManager.setViewPageSlide(isSlide);
+    }
+
+    @Override
+    public void recordDuration(String id, int pageIndex, long pro, long total) {
+        //进度回调
+
+    }
+
+    @Override
+    public void onCheck(Object str, Object obj) {
+
+    }
+
+    @Override
+    public VideoPlayVo getVideoPlayData(int pageIndex) {
+        return null;
+    }
+
+    @Override
+    public Object getVideoDetailsData(int pageIndex) {
+        return null;
+    }
+
+    @Override
+    public void isDataReq(boolean isRefresh) {
+
+    }
+    //
+}
---
+++ option/src/main/java/media/library/player/view/VideoPageRl2.java
@@ -0,0 +1,73 @@
+package media.library.player.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+
+public class VideoPageRl2 extends RelativeLayout {
+
+    public VideoPageRl2(Context context) {
+        super(context);
+    }
+
+    public VideoPageRl2(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VideoPageRl2(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public VideoPageRl2(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    private boolean isVertical = false;
+    private float initialX = 0f;
+    private float initialY = 0f;
+
+    private void handleInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initialX = ev.getX();
+                initialY = ev.getY();
+                isVertical = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (isVertical) {
+                    return;
+                }
+                float dx = ev.getX() - initialX;
+                float dy = ev.getY() - initialY;
+                initialX = ev.getX();
+                initialY = ev.getY();
+                if (Math.abs(dy) > Math.abs(dx)) {
+                    //垂直滑动
+                    isVertical = true;
+                    if (listener != null) {
+                        listener.onVertical();
+                    }
+
+                }
+                break;
+        }
+    }
+
+    private OnVerticalListener listener;
+
+    public void setOnVerticalListener(OnVerticalListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnVerticalListener {
+        void onVertical();
+    }
+
+}
